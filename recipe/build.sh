7f70a27e1b6cc959f051dd7c82df771817423c24
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-19 10:02:26 -0800

    try different strategy

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,29 +9,29 @@ export OPENSSL_NO_VENDOR=1
 export LIBGIT2_NO_VENDOR=1
 export PKG_CONFIG_ALLOW_CROSS=1
 
-if [[ ${target_platform} == "osx-arm64" ]]; then
-tee ${BUILD_PREFIX}/bin/cc_shim << EOF
-#!/bin/sh
-if [[ "\$@" =~ build-script* ]]; then
-    exec \${CC_FOR_BUILD} \${CFLAGS//\${PREFIX}/\${BUILD_PREFIX}} \${LDFLAGS//\${PREFIX}/\${BUILD_PREFIX}} "\$@"
-else
-    exec \${CC} \${CFLAGS} \${LDFLAGS} "\$@"
-fi
-EOF
-else
-tee ${BUILD_PREFIX}/bin/cc_shim << EOF
-#!/bin/sh
-exec ${CC} ${CFLAGS} ${LDFLAGS} "\$@"
-EOF
-fi
-chmod +x ${BUILD_PREFIX}/bin/cc_shim
-export CC=${BUILD_PREFIX}/bin/cc_shim
+# if [[ ${target_platform} == "osx-arm64" ]]; then
+# tee ${BUILD_PREFIX}/bin/cc_shim << EOF
+# #!/bin/sh
+# if [[ "\$@" =~ build-script* ]]; then
+#     exec \${CC_FOR_BUILD} \${CFLAGS//\${PREFIX}/\${BUILD_PREFIX}} \${LDFLAGS//\${PREFIX}/\${BUILD_PREFIX}} "\$@"
+# else
+#     exec \${CC} \${CFLAGS} \${LDFLAGS} "\$@"
+# fi
+# EOF
+# else
+# tee ${BUILD_PREFIX}/bin/cc_shim << EOF
+# #!/bin/sh
+# exec ${CC} ${CFLAGS} ${LDFLAGS} "\$@"
+# EOF
+# fi
+# chmod +x ${BUILD_PREFIX}/bin/cc_shim
+# export CC=${BUILD_PREFIX}/bin/cc_shim
 
 # export PKG_CONFIG_PATH="${BUILD_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}"
-# if [[ ${target_platform} =~ .*osx.* ]]; then
-#     mkdir -p ${SRC_DIR}/target/release/deps
-#     ln -sf ${BUILD_PREFIX}/lib/libgit2* ${SRC_DIR}/target/release/deps
-# fi
+if [[ ${target_platform} =~ .*osx.* ]]; then
+    mkdir -p ${SRC_DIR}/target/release/deps
+    ln -sf ${BUILD_PREFIX}/lib/libgit2* ${SRC_DIR}/target/release/deps
+fi
 
 cargo-bundle-licenses \
     --format yaml \
